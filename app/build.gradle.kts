3f3dc01908dc465dcb1a8b636d4ef5b2f5b7fbc6
Bintang Nasution <sumbercodeku@gmail.com>
2023-12-05 19:49:06 +0700

    Implement UI for startlive record

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.camera:camera-view:1.3.0")
+    implementation("androidx.camera:camera-video:1.3.0")
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-extensions:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-viewfinder:1.4.0-alpha02")
+
+
 }
