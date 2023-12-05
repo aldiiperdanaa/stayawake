6bcfa3c53f71dc8f45441ac789afee67e0c0a5d1
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2023-12-06 01:11:12 +0800

    Onboarding screen

--- app/src/main/java/com/aldiperdana/mobilestayawake/SplashScreenActivity.kt
+++ app/src/main/java/com/aldiperdana/mobilestayawake/SplashScreenActivity.kt
@@ -17,7 +17,7 @@ class SplashScreenActivity : AppCompatActivity() {
         )
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, OnboardingScreenActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
