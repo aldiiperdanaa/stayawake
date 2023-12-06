13781804554a3c929e0bfcf5bdce76a412ccfc0b
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2023-12-06 23:52:12 +0800

    Register screen

--- app/src/main/java/com/aldiperdana/mobilestayawake/NewUserActivity.kt
+++ app/src/main/java/com/aldiperdana/mobilestayawake/NewUserActivity.kt
@@ -2,10 +2,21 @@ package com.aldiperdana.mobilestayawake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class NewUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_user)
+
+        val bottom_sheet: TextView = findViewById(R.id.tv_referral)
+        bottom_sheet.setOnClickListener{
+            val view: View = layoutInflater.inflate(R.layout.item_bottom_sheet_referral_code, null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 }
