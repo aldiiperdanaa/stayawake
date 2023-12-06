93eb22fdd5f48baa0ec82681ca06ba9bb4c4615d
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2023-12-06 08:10:31 +0800

    Login screen

---
+++ app/src/main/java/com/aldiperdana/mobilestayawake/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.aldiperdana.mobilestayawake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aldiperdana.mobilestayawake.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
