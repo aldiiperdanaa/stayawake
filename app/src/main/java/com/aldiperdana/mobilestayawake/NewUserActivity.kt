3d2c26b97a622f376b6786c278b8f98f9eb8e736
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2023-12-07 10:09:30 +0800

    Repair radio button multi text

--- app/src/main/java/com/aldiperdana/mobilestayawake/NewUserActivity.kt
+++ app/src/main/java/com/aldiperdana/mobilestayawake/NewUserActivity.kt
@@ -3,13 +3,40 @@ package com.aldiperdana.mobilestayawake
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class NewUserActivity : AppCompatActivity() {
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var personalRadioButton: RadioButton
+    private lateinit var companyRadioButton: RadioButton
+    private lateinit var textViewPersonal: TextView
+    private lateinit var textViewCompany: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_user)
+
+        radioGroup = findViewById(R.id.radioGroup)
+        personalRadioButton = findViewById(R.id.personalRadioButton)
+        companyRadioButton = findViewById(R.id.companyRadioButton)
+        textViewPersonal = findViewById(R.id.textViewPersonal)
+        textViewCompany = findViewById(R.id.textViewCompany)
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                personalRadioButton.id -> {
+                    textViewPersonal.setTextColor(resources.getColor(R.color.primary80))
+                    textViewCompany.setTextColor(resources.getColor(R.color.textSecondary))
+                }
+                companyRadioButton.id -> {
+                    textViewCompany.setTextColor(resources.getColor(R.color.primary80))
+                    textViewPersonal.setTextColor(resources.getColor(R.color.textSecondary))
+                }
+            }
+        }
 
         val bottom_sheet: TextView = findViewById(R.id.tv_referral)
         bottom_sheet.setOnClickListener{
