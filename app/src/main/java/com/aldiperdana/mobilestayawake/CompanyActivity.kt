72bcb8a78817e755fcbe4e974d189c6a77220ae6
Bintang Nasution <sumbercodeku@gmail.com>
2023-12-07 14:04:57 +0700

    Configuration CompanyActivity Frame Moves

--- app/src/main/java/com/aldiperdana/mobilestayawake/CompanyActivity.kt
+++ app/src/main/java/com/aldiperdana/mobilestayawake/CompanyActivity.kt
@@ -1,11 +1,20 @@
 package com.aldiperdana.mobilestayawake
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aldiperdana.mobilestayawake.databinding.ActivityCompanyBinding
 
 class CompanyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCompanyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_company)
+        binding = ActivityCompanyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnContinue.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
