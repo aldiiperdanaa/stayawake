eb9adfc9eb1f68d97ea71c00f883585bce838082
Ahmad Zaki Firdaus <ahmzakif@gmail.com>
2023-12-10 15:32:21 +0700

    Fix Score

--- main.py
+++ main.py
@@ -14,58 +14,59 @@ model = load_model(MODEL_V1)
 mixer.init()
 sound = mixer.Sound(r'models/assets/alarm.wav')
 cap = cv2.VideoCapture(0)
-Score = 0
+font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+score = 0
 
-while True:
+while(True):
     ret, frame = cap.read()
-    height, width = frame.shape[0:2]
+    height,width = frame.shape[:2]
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
-    eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
+    faces = face_cascade.detectMultiScale(gray,minNeighbors = 3,scaleFactor = 1.1,minSize=(25,25))
+    eyes = eye_cascade.detectMultiScale(gray,minNeighbors = 1,scaleFactor = 1.1)
 
-    cv2.rectangle(frame, (0, height-50), (200, height), (0, 0, 0), thickness=cv2.FILLED)
+    cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, pt1=(x, y), pt2=(x+w, y+h), color=(255, 0, 0), thickness=3)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (255,0,0) , 3 )
 
-        for (ex, ey, ew, eh) in eyes:
-            eye = frame[ey:ey+eh, ex:ex+w]
-            eye = cv2.resize(eye, (80, 80))
-            eye = eye / 255
-            eye = eye.reshape(80, 80, 3)
-            eye = np.expand_dims(eye, axis=0)
-            prediction = model.predict(eye)
+    for (x,y,w,h) in eyes:
 
-            #Condition for Drowsy
-            if prediction[0][0] > 0.30:
-                cv2.putText(frame, 'Drowsy', (10, height-20), fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=1,
-                            color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
-                cv2.putText(frame, 'Score'+str(Score), (150, height-20), fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                            fontScale=1, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
-                Score = Score + 1
+        eye = frame[y:y+h,x:x+w]
+        #eye = cv2.cvtColor(eye,cv2.COLOR_BGR2GRAY)
+        eye = cv2.resize(eye,(80,80))
+        eye = eye/255
+        eye = eye.reshape(80,80,3)
+        eye = np.expand_dims(eye,axis=0)
+        prediction = model.predict(eye)
+            
+    #Condition for Close
+        if prediction[0][0]>0.30:
+            cv2.putText(frame,"Drowsy",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+            cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+            score=score+1
+                
+            if(score < 10):
+                try:
+                    sound.play()
+                except:
+                    pass
 
-                if Score < 10:
-                    try:
-                        sound.play()
-                    except:
-                        pass
+        #Condition for Open
+        elif prediction[0][1] > 0.15:
+            score = score - 1
+            if (score < 0):
+                score = 0
+            cv2.putText(frame,"Awake",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+                
+            cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
 
-            #Condition for Awake                
-            elif prediction[0][1] > 0.20:
-                cv2.putText(frame, 'Awake', (10, height-20), fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL, fontScale=1,
-                            color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
-                cv2.putText(frame, 'Score'+str(Score), (150, height-20), fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
-                            fontScale=1, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
-                Score = Score - 1
-
-                if Score < 0:
-                    Score = 0
-        
-    cv2.imshow('StayAwake', frame)
-    if cv2.waitKey(33) & 0xFF == ord('q'):
+    cv2.imshow('StayAwake',frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+        
 cap.release()
 cv2.destroyAllWindows()
+
